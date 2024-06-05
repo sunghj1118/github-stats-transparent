0e9419c15e8a2f67019a6b622f43514d6ae4d2a3
sunghj1118 <sunghj1118@gmail.com>
2024-06-06 00:43:10 +0900

    fix: add logging

--- generate_images.py
+++ generate_images.py
@@ -3,11 +3,13 @@
 import asyncio
 import os
 import re
-
 import aiohttp
+import logging
 
 from github_stats import Stats
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 ################################################################################
 # Helper Functions
@@ -19,7 +21,6 @@ def generate_output_folder() -> None:
     """
     if not os.path.isdir("generated"):
         os.mkdir("generated")
-
 
 ################################################################################
 # Individual Image Generation Functions
@@ -36,8 +37,7 @@ async def generate_overview(s: Stats) -> None:
     output = re.sub("{{ name }}", await s.name, output)
     output = re.sub("{{ stars }}", f"{await s.stargazers:,}", output)
     output = re.sub("{{ forks }}", f"{await s.forks:,}", output)
-    output = re.sub("{{ contributions }}", f"{await s.total_contributions:,}",
-                    output)
+    output = re.sub("{{ contributions }}", f"{await s.total_contributions:,}", output)
     changed = (await s.lines_changed)[0] + (await s.lines_changed)[1]
     output = re.sub("{{ lines_changed }}", f"{changed:,}", output)
     output = re.sub("{{ views }}", f"{await s.views:,}", output)
@@ -46,7 +46,6 @@ async def generate_overview(s: Stats) -> None:
     generate_output_folder()
     with open("generated/overview.svg", "w") as f:
         f.write(output)
-
 
 async def generate_languages(s: Stats) -> None:
     """
@@ -58,8 +57,7 @@ async def generate_languages(s: Stats) -> None:
 
     progress = ""
     lang_list = ""
-    sorted_languages = sorted((await s.languages).items(), reverse=True,
-                              key=lambda t: t[1].get("size"))
+    sorted_languages = sorted((await s.languages).items(), reverse=True, key=lambda t: t[1].get("size"))
     delay_between = 150
     for i, (lang, data) in enumerate(sorted_languages):
         color = data.get("color")
@@ -81,7 +79,6 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 <span class="lang">{lang}</span>
 <span class="percent">{data.get("prop", 0):0.2f}%</span>
 </li>
-
 """
 
     output = re.sub(r"{{ progress }}", progress, output)
@@ -90,7 +87,6 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
     generate_output_folder()
     with open("generated/languages.svg", "w") as f:
         f.write(output)
-
 
 ################################################################################
 # Main Function
@@ -102,22 +98,17 @@ async def main() -> None:
     """
     access_token = os.getenv("ACCESS_TOKEN")
     if not access_token:
-        # access_token = os.getenv("GITHUB_TOKEN")
         raise Exception("A personal access token is required to proceed!")
     user = os.getenv("GITHUB_ACTOR")
     exclude_repos = os.getenv("EXCLUDED")
-    exclude_repos = ({x.strip() for x in exclude_repos.split(",")}
-                     if exclude_repos else None)
+    exclude_repos = ({x.strip() for x in exclude_repos.split(",")} if exclude_repos else None)
     exclude_langs = os.getenv("EXCLUDED_LANGS")
-    exclude_langs = ({x.strip() for x in exclude_langs.split(",")}
-                     if exclude_langs else None)
+    exclude_langs = ({x.strip() for x in exclude_langs.split(",")} if exclude_langs else None)
     consider_forked_repos = len(os.getenv("COUNT_STATS_FROM_FORKS")) != 0
-    async with aiohttp.ClientSession() as session:
-        s = Stats(user, access_token, session, exclude_repos=exclude_repos,
-                  exclude_langs=exclude_langs,
-                  consider_forked_repos=consider_forked_repos)
-        await asyncio.gather(generate_languages(s), generate_overview(s))
 
+    async with aiohttp.ClientSession() as session:
+        s = Stats(user, access_token, session, exclude_repos=exclude_repos, exclude_langs=exclude_langs, consider_forked_repos=consider_forked_repos)
+        await asyncio.gather(generate_languages(s), generate_overview(s))
 
 if __name__ == "__main__":
     asyncio.run(main())
